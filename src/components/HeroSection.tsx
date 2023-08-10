6320e4b153e6b4388079ce14b1b92b6a88153987
B-RunP <brianpurnama157@gmail.om>
2023-08-10 13:03:26 +0700

    First Commit

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-scroll";
+import { HiArrowDown } from "react-icons/hi";
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-24 md:flex-row md:space-x-4 md:text-left">
+        <div className="md:mt-2 md:w-1/2">
+          <Image
+            className="rounded-full shadow-xl"
+            src="/me.jpeg"
+            alt="me"
+            width={325}
+            height={325}
+          />
+        </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="font-bold text-4xl mt-6 md:text-7xl md:mt-0">
+            Hi, I&#39;m Brian!
+          </h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            I am a person who is very interested in Frontend Developer and
+            website development developer
+          </p>
+          <Link
+            to="projects"
+            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}>
+            Projects
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row justify-center">
+        <Link
+          to="about"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}>
+          <HiArrowDown size={35} className="animate-bounce" />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
